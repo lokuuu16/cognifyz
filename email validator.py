4086a90b58e22e251f7ffb000636218027347acd
lokuuu16 <153181730+lokuuu16@users.noreply.github.com>
2024-02-05 14:03:51 +0530

    Update email validator.py

--- email validator.py
+++ email validator.py
@@ -1,1 +1,12 @@
+temp=float(input("enter the temperature value:"))
+unit_o_m=input("enter the unit of the entered value's temperature(C/F):")
+if unit_o_m=="C":
+    F=round((temp*(9/5))+32,2)
+    print("The equivalent fahrenhent value is:",F,"fahrenheit")
 
+elif unit_o_m=="F":
+    C=round((temp-32)*(5/9),2)
+    print("The equivalent celsius value is:",C,"celsius")
+
+else:
+    print("enter valid unit of temperature(C/F)")
